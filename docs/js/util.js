006839819e85a646f01d1edee7acf1b2a546cbbd
maesungH1418 <129656245+maesung1418@users.noreply.github.com>
2023-06-18 16:06:51 +0900

    post 3

--- docs/js/util.js
+++ docs/js/util.js
@@ -1,12 +1,24 @@
 escapeSeqChar = "#";
 escapeSeqDict = {
     "n": "<br>",
-    "q": "\""
+    "dn": "<br><br>",
+    "q": "\"",
+    "CS": "<div class=\"codespace\">",
+    "ED": "</div>"
 }
 function escapeSeqFormat(txt) {
     Object.keys(escapeSeqDict).forEach(key => {
         var target = escapeSeqChar + key;
         txt = txt.replaceAll(target, escapeSeqDict[key]);
+    })
+    return txt;
+}
+
+
+function format(txt) {
+    Object.keys(escapeSeqDict).forEach(key => {
+        var target = escapeSeqDict[key];
+        txt = txt.replaceAll(target, escapeSeqChar + key);
     })
     return txt;
 }
